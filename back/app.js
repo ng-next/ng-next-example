9e86b3b8ec9d75846aebca2d66a71fad6c5f587f
Andreas Grimm <schaeferitsolutions@gmail.com>
2015-02-03 21:12:00 +0100

    fix style issues

--- back/app.js
+++ back/app.js
@@ -9,50 +9,54 @@ app.set( 'port', process.env.PORT || 3000 );
 app.use( logger( 'dev' ));
 
 if ( app.get( 'env' ) === 'development' ) {
-  app.use( express.static( path.join( __dirname, '../front/main' ) ) );
-  app.use( '/test-doubles', express.static( path.join( __dirname, '../front/test/unit/test-doubles' ) ) );
+  app.use( express.static( path.join( __dirname, '../front/main' )));
+  app.use( '/test-doubles', express.static(
+    path.join( __dirname, '../front/test/unit/test-doubles' )));
 } else if ( app.get( 'env' ) === 'build-debug' ) {
-  app.use( express.static( path.join( __dirname, '../public' ) ) );
-  app.use( '/jspm_packages', express.static( path.join( __dirname, '../front/main/jspm_packages' ) ) );
+  app.use( express.static( path.join( __dirname, '../public' )));
+  app.use( '/jspm_packages', express.static(
+    path.join( __dirname, '../front/main/jspm_packages' )));
 } else {
-  app.use( express.static( path.join( __dirname, '../public' ) ) );
-  app.use( '/jspm_packages', express.static( path.join( __dirname, '../front/main/jspm_packages' ) ) );
+  app.use( express.static( path.join( __dirname, '../public' )));
+  app.use( '/jspm_packages', express.static(
+    path.join( __dirname, '../front/main/jspm_packages' )));
 }
 
 ///**
 // * ERROR HANDLING
 // */
 ///// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use( function ( req, res, next ) {
+  var err = new Error( 'Not Found' );
   err.status = 404;
-  next(err);
+  next( err );
 });
 
 ///// error handlers
 //
 //// development error handler
 //// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if ( app.get( 'env' ) === 'development' ) {
+  app.use( function ( err, req, res, next ) {
+    res.status( err.status || 500 );
+    res.render( 'error', {
+      message : err.message,
+      error   : err
     });
   });
 }
 
 //// production error handler
 //// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+app.use( function ( err, req, res, next ) {
+  res.status( err.status || 500 );
+  res.render( 'error', {
+    message : err.message,
+    error   : {}
   });
 });
 
 app.listen( app.get( 'port' ), function () {
-  console.log( 'Express server listening in env ' + env + ' on port ' + app.get( 'port' ));
+  console.log( 'Express server listening in env ' + env +
+    ' on port ' + app.get( 'port' ));
 });
